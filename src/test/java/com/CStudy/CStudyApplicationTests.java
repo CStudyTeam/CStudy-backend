4e384cff674a185f9056cd24d2c06699c011073a
wnsgk <unwnsgk1282@naver.com>
2023-05-29 16:17:32 +0900

    initial commit

---
+++ src/test/java/com/CStudy/CStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
