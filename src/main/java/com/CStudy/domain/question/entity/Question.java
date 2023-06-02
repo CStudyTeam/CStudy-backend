cb401f6be5778f15d88ef5f576d46879bbfe2bbc
KMGeon <pos04167@kakao.com>
2023-06-03 00:06:47 +0900

    ♻️refactor: 문제 관계 추가

--- src/main/java/com/CStudy/domain/question/entity/Question.java
+++ src/main/java/com/CStudy/domain/question/entity/Question.java
@@ -1,10 +1,19 @@
 package com.CStudy.domain.question.entity;
 
+import com.CStudy.domain.choice.entity.Choice;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class Question {
 
     @Id
@@ -27,4 +36,30 @@ public class Question {
             cascade = CascadeType.ALL
     )
     Set<MemberQuestion> questions = new HashSet<>();
+
+    @OneToMany(
+            mappedBy = "question",
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL
+    )
+    List<Choice>choices = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Builder
+    public Question(Long id, String title, String description, String explain, Set<MemberQuestion> questions, List<Choice> choices, Category category) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.explain = explain;
+        this.questions = questions;
+        this.choices = choices;
+        this.category = category;
+    }
+    public void setChoices(List<Choice> choices) {
+        this.choices = choices;
+    }
+
 }
