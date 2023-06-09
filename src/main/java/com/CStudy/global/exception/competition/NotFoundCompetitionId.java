d52207285f132d3ecfdab953fa92b4e189bed243
KMGeon <pos04167@kakao.com>
2023-06-09 23:55:04 +0900

    ✨feat: competition Exception 추가

---
+++ src/main/java/com/CStudy/global/exception/competition/NotFoundCompetitionId.java
@@ -0,0 +1,7 @@
+package com.CStudy.global.exception.competition;
+
+public class NotFoundCompetitionId extends RuntimeException {
+    public NotFoundCompetitionId(Long competitionId) {
+        super("Not Found Competition About CompetitionId"+competitionId);
+    }
+}
