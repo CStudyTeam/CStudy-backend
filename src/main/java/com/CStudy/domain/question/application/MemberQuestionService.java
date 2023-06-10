0ab156d64506e98f10561e2f380a1cd94e0d166f
KMGeon <pos04167@kakao.com>
2023-06-10 22:41:30 +0900

    ♻️refactor: 중복 검증하는 로직 추가

--- src/main/java/com/CStudy/domain/question/application/MemberQuestionService.java
+++ src/main/java/com/CStudy/domain/question/application/MemberQuestionService.java
@@ -3,4 +3,5 @@ package com.CStudy.domain.question.application;
 public interface MemberQuestionService {
     void findMemberAndMemberQuestionSuccess(Long memberId, Long questionId, int choiceNumber);
     void findMemberAndMemberQuestionFail(Long memberId, Long questionId, int choiceNumber);
+    void findByQuestionAboutMemberIdAndQuestionId(Long memberId, Long questionId);
 }
