dc83e125cbd72f940f72378a564e62364f63539b
wnsgk <unwnsgk1282@naver.com>
2023-06-17 19:25:25 +0900

    ♻️refactor: Competition Swagger 추가

--- src/main/java/com/CStudy/domain/competition/controller/CompetitionController.java
+++ src/main/java/com/CStudy/domain/competition/controller/CompetitionController.java
@@ -24,6 +24,7 @@ import org.springframework.data.web.SortDefault;
 import org.springframework.data.web.SortDefault.SortDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
 @Tag(name = "Competition(경기 API)", description = "경기 생성 및 Get")
 @Slf4j
 @RestController
@@ -37,7 +38,8 @@ public class CompetitionController {
     public CompetitionController(
             CompetitionService competitionService,
             MemberCompetitionService memberCompetitionService,
-        CompetitionScoreService competitionScoreService) {
+            CompetitionScoreService competitionScoreService
+    ) {
         this.competitionService = competitionService;
         this.memberCompetitionService = memberCompetitionService;
         this.competitionScoreService = competitionScoreService;
@@ -56,55 +58,93 @@ public class CompetitionController {
         competitionService.createCompetition(createCompetitionRequestDto);
     }
 
+    @Operation(summary = "대회 참여하기", description = "대회 id를 이용해 로그인 한 유저가 해당 대회 참여하기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "대회 참여하기 성공"),
+            @ApiResponse(responseCode = "400", description = "대회 참여 인원 초과")
+    })
     @PostMapping("join/competition/{competitionId}")
     @ResponseStatus(HttpStatus.CREATED)
     public void joinCompetitionById(
+            @Parameter(hidden = true)
             @IfLogin LoginUserDto loginUserDto,
+            @Parameter(description = "대회 id")
             @PathVariable(name = "competitionId") Long competitionId
     ) {
         memberCompetitionService.joinCompetition(loginUserDto, competitionId);
     }
 
+    @Operation(summary = "대회 정보", description = "대회 id를 이용해 대회 정보 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "대회 정보 조회 성공"),
+            @ApiResponse(responseCode = "400", description = "대회 정보 조회 실패")
+    })
     @GetMapping("competition/{competitionId}")
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     public CompetitionResponseDto getCompetition(
+            @Parameter(description = "대회 id")
             @PathVariable Long competitionId
     ) {
         return competitionService.getCompetition(competitionId);
     }
 
+    @Operation(summary = "참여 가능 대회 리스트", description = "참여 가능 대회 리스트")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "대회 리스트 조회 성공"),
+            @ApiResponse(responseCode = "400", description = "대회 리스트 조회 실패")
+    })
     @GetMapping("competition/list")
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     public Page<CompetitionListResponseDto> getAvailableCompetition(
+            @Parameter(description = "page: 페이지 번호, size: 한 페이지 문제 수.")
             @PageableDefault(sort = {"competitionStart"}, direction = Direction.ASC) Pageable pageable
     ) {
         return competitionService.getCompetitionList(false, pageable);
     }
 
+    @Operation(summary = "종료된 대회 리스트", description = "종료된 대회 리스트")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "대회 리스트 조회 성공"),
+            @ApiResponse(responseCode = "400", description = "대회 리스트 조회 실패")
+    })
     @GetMapping("competition/finish/list")
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     public Page<CompetitionListResponseDto> getFinishCompetition(
+            @Parameter(description = "page: 페이지 번호, size: 한 페이지 문제 수.")
             @PageableDefault(sort = {"competitionStart"}, direction = Direction.DESC) Pageable pageable
     ) {
         return competitionService.getCompetitionList(true, pageable);
     }
 
+    @Operation(summary = "대회 랭킹", description = "대회 id를 이용해 대회에 참여한 회원들의 랭킹 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "랭킹 조회 성공"),
+            @ApiResponse(responseCode = "400", description = "랭킹 조회 실패")
+    })
     @GetMapping("competition/ranking/{id}")
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     public Page<CompetitionRankingResponseDto> getRanking(
+            @Parameter(description = "page: 페이지 번호, size: 한 페이지 문제 수.")
             @PageableDefault @SortDefaults({
                 @SortDefault(sort = "score", direction = Direction.DESC),
                 @SortDefault(sort = "endTime",direction = Direction.ASC)
             }) Pageable pageable,
+            @Parameter(description = "대회 id")
             @PathVariable Long id
     ) {
         return competitionService.getCompetitionRanking(id, pageable);
     }
 
+    @Operation(summary = "대회 답안 제출", description = "문제를 다 풀고 답안 제출")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "제출 성공")
+    })
     @PostMapping("competition/submit")
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void submit(
+            @Parameter(description = "competitionId: 대회 id, endTime: 제출한 시간, questions.questionId: 문제 id, questions.choiceNumber: 고른 선택지 번호")
             @RequestBody CompetitionScoreRequestDto requestDto,
+            @Parameter(hidden = true)
             @IfLogin LoginUserDto loginUserDto
     ) {
         competitionScoreService.scoring(requestDto, loginUserDto);
