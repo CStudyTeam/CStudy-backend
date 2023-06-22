f1c9b0115932c39135b6c68db4552960b9e66628
KMGeon <pos04167@kakao.com>
2023-06-22 18:28:56 +0900

    ♻️refactor: 테스트 코드 및 코드 컨벤션 수정

--- src/main/java/com/CStudy/domain/competition/dto/request/CreateCompetitionRequestDto.java
+++ src/main/java/com/CStudy/domain/competition/dto/request/CreateCompetitionRequestDto.java
@@ -1,23 +1,14 @@
 package com.CStudy.domain.competition.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class CreateCompetitionRequestDto {
+public class createCompetitionRequestDto {
     private String competitionTitle;
     private int participants;
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
-    private LocalDateTime competitionStart;
-
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime competitionEnd;
 }
---
+++ src/test/java/com/CStudy/domain/competition/application/impl/MemberCompetitionServiceImplTest.java
@@ -0,0 +1,142 @@
+package com.CStudy.domain.competition.application.impl;
+
+import com.CStudy.domain.competition.application.CompetitionService;
+import com.CStudy.domain.competition.application.MemberCompetitionService;
+import com.CStudy.domain.competition.dto.request.createCompetitionRequestDto;
+import com.CStudy.domain.competition.entity.Competition;
+import com.CStudy.domain.competition.repository.CompetitionRepository;
+import com.CStudy.domain.member.application.MemberService;
+import com.CStudy.domain.member.dto.request.MemberSignupRequest;
+import com.CStudy.domain.member.repository.MemberRepository;
+import com.CStudy.global.exception.competition.NotFoundCompetitionId;
+import com.CStudy.global.util.LoginUserDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("local")
+//@Transactional
+class MemberCompetitionServiceImplTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private CompetitionService competitionService;
+
+    @Autowired
+    private MemberCompetitionService memberCompetitionService;
+
+    @Autowired
+    private CompetitionRepository competitionRepository;
+
+    private static final String EMAIL = "test1213@email.com";
+    private static final String PASSWORD = "test1234!";
+    private static final String NAME = "김무건";
+
+    @BeforeEach
+    void setUp() {
+
+        MemberSignupRequest memberSignupRequest = MemberSignupRequest.builder()
+                .email(EMAIL)
+                .password(PASSWORD)
+                .name(NAME)
+                .build();
+        memberService.signUp(memberSignupRequest);
+
+        MemberSignupRequest memberSignupRequest2 = MemberSignupRequest.builder()
+                .email("EMAIL")
+                .password("PASSWORD")
+                .name("NAME")
+                .build();
+
+        memberService.signUp(memberSignupRequest2);
+
+
+        createCompetitionRequestDto requestDto = createCompetitionRequestDto.builder()
+                .competitionTitle("CS 대회")
+                .participants(5)
+                .competitionEnd(LocalDateTime.now().plusHours(1))
+                .build();
+
+        competitionService.createCompetition(requestDto);
+    }
+
+    @Test
+    @DisplayName("싱글 스레드 환경에서 대회 참가 조회")
+    public void findCompetitionAboutJoinCompetitionWithSingleThread() throws Exception {
+        //given
+        LoginUserDto loginUserDto = LoginUserDto.builder()
+                .memberId(2L)
+                .build();
+        //when
+        memberCompetitionService.joinCompetition(loginUserDto, 1L);
+
+        //Then
+        Competition competition = competitionRepository.findById(1L)
+                .orElseThrow(() -> new NotFoundCompetitionId(1L));
+
+        assertThat(competition.getParticipants()).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("싱글 스레드 환경에서 대회 실패 참가 조회")
+    public void findCompetitionAboutInvalidJoinCompetitionWithSingleThread() throws Exception {
+        //given
+        LoginUserDto loginUserDto = LoginUserDto.builder()
+                .memberId(2L)
+                .build();
+        //when
+        memberCompetitionService.joinCompetition(loginUserDto, 1L);
+
+        //Then
+        Competition competition = competitionRepository.findById(1L)
+                .orElseThrow(() -> new NotFoundCompetitionId(1L));
+
+        assertThat(competition.getParticipants()).isNotEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("멀티쓰레드 환경에서 테스트")
+    public void studyMemberCreateJoinMemberWithMultiThread() throws Exception {
+
+        LoginUserDto userDto = LoginUserDto.builder()
+                .memberId(1L)
+                .build();
+
+        int threadCount = 100;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    memberCompetitionService.joinCompetition(userDto, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+        Competition competition = competitionRepository.findById(1L)
+                .orElseThrow();
+
+        assertThat(competition.getParticipants()).isEqualTo(0);
+    }
+}
