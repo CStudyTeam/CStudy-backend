bc4c7af74ea1673273cea6a9853d4ce35c12045a
KMGeon <pos04167@kakao.com>
2023-07-25 03:09:18 +0900

    ♻️refactor: s3

--- src/main/java/com/CStudy/domain/member/application/FileService.java
+++ src/main/java/com/CStudy/domain/member/application/FileService.java
@@ -1,12 +1,12 @@
-package com.CStudy.domain.member.application;
-
-import com.CStudy.global.util.LoginUserDto;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
-
-public interface FileService {
-    List<String> uploadFiles(MultipartFile[] multipartFileList, LoginUserDto loginUserDto) throws Exception;
-
-    byte[] getImageBytes(LoginUserDto loginUserDto);
-}
+//package com.CStudy.domain.member.application;
+//
+//import com.CStudy.global.util.LoginUserDto;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.util.List;
+//
+//public interface FileService {
+//    List<String> uploadFiles(MultipartFile[] multipartFileList, LoginUserDto loginUserDto) throws Exception;
+//
+//    byte[] getImageBytes(LoginUserDto loginUserDto);
+//}
