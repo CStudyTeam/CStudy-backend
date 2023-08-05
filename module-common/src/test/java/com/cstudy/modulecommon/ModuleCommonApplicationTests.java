2aa1e6a82db94556ad6d0309ed958fd7cd5a2a7c
KMGeon <pos04167@kakao.com>
2023-08-06 08:51:26 +0900

    ♻️refactor: develop/v.1.3.2 multi module

---
+++ module-common/src/test/java/com/cstudy/modulecommon/ModuleCommonApplicationTests.java
@@ -0,0 +1,15 @@
+package com.cstudy.modulecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("local")
+class ModuleCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
