c297b7391b1169785efaa1e9a8d9eeea3cf90afb
KMGeon <pos04167@kakao.com>
2023-08-05 23:09:03 +0900

    ♻️refactor: 코드 컨벤션 수정

--- src/main/java/com/CStudy/CStudyApplication.java
+++ src/main/java/com/CStudy/CStudyApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CStudyApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CStudyApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CStudyApplication.class, args);
+    }
 }
